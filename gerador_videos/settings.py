f75a2707b1b077da8cfe114fc978e7157fb4ed68
Lucas Silva <lucaspessoal211@gmail.com>
2025-08-25 00:33:01 -0300

    Depois da atualiação, em andamento ainda

--- gerador_videos/settings.py
+++ gerador_videos/settings.py
@@ -97,7 +97,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
@@ -121,10 +124,10 @@ APPEND_SLASH = True
 # CONFIGURAÇÕES DE E-MAIL
 # ================================================================
 # Lê as credenciais do e-mail do arquivo .env para segurança
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' # <-- ADICIONE ESTA LINHA PARA TESTE
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
